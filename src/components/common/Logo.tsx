b86e65bbd5b2ec529eeef29f1504eee5b1bce0fd
Rana <rana.adel.farag@gmail.com>
2026-02-02 17:12:30 +0200

    edit

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -2,12 +2,8 @@ import { Link } from "react-router";
 
 export default function Logo() {
   return (
-    <Link to="/" className="flex h-12  items-center justify-center">
-      <img
-        src="/logo.png"
-        alt="logo"
-        className="h-full w-full object-contain"
-      />
+    <Link to="/" className="flex h-12 items-center justify-center">
+      <img src="/logo.png" alt="logo" className="h-full w-full" />
     </Link>
   );
 }
